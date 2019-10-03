5f9708f1175f1e6618b043b830a0d93fd75571c0
Nikiforov Viacheslav <55716893+slabuke@users.noreply.github.com>
2019-10-03 22:23:34 +0300

    Create SheduledJob.ps1

---
+++ Scenario/SheduledJob.ps1
@@ -0,0 +1,3 @@
+﻿$t = New-JobTrigger -Once -At 9:00 -RepetitionInterval 00:10 -RepetitionDuration 10:00
+$cred = Get-Credential Администратор
+Register-ScheduledJob -Name Script_3 -FilePath D:\Scenario\Script_3.ps1 -Trigger $t -Credential $cred -RunAs32
