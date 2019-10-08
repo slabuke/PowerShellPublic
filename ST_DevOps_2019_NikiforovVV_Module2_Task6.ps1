a75e142e67cb9604e7cc43606e38e94d0dd571f4
Nikiforov Viacheslav <55716893+slabuke@users.noreply.github.com>
2019-10-09 01:33:39 +0300

    Update ST_DevOps_2019_NikiforovVV_Module2_Task6.ps1

--- ST_DevOps_2019_NikiforovVV_Module2_Task6.ps1
+++ ST_DevOps_2019_NikiforovVV_Module2_Task6.ps1
@@ -29,13 +29,54 @@ IPConfig /all
 
 
 #1.4.	Расшарить папку на компьютере
+Invoke-Command -ScriptBlock {
+    IF (-not(TEST-PATH \\vm2\Users\Administrator\Desktop\TESTSHARE)) 
+                  {
+                    New-Item -Type Directory -Path \\VM2\Users\Administrator\Desktop\TESTSHARE
+                  }
+    
+    $Shares = [WMICLASS]”WIN32_Share”
+    $Shares.Create(“\\VM2\Users\Administrator\Desktop\TESTSHARE”,”TEST”,0,24)
+    
+    } -ComputerName VM2 -Credential Administrator
+    <#Invoke-Command -ScriptBlock {
+        (Get-WmiObject -List -ComputerName .| Where-Object -FilterScript {$_.Name –eq "Win32_Share"}).InvokeMethod("Create",("C:\M2T2_Nikiforov","TempShare",0,25,"test share of the temp folder"))
+        } -ComputerName VM1 -Credential Administrator#>
+
 #1.5.	Удалить шару из п.1.4
+Invoke-Command -ScriptBlock {
+
+    Remove-SmbShare -Name "TESTSHARE" -Force
+    Remove-Item -Path "\\VM2\Users\Administrator\Desktop\TESTSHARE" -Force
+    
+} -ComputerName VM1 -Credential Administrator
+
 #1.6.	Скрипт входными параметрами которого являются Маска подсети и два ip-адреса. Результат  – сообщение (ответ) в одной ли подсети эти адреса.
 #2.	Работа с Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName  Microsoft-Hyper-V-Management-PowerShell
+
+Enable-WindowsOptionalFeature -Online -FeatureName  Microsoft-Hyper-V-Tools-All
+
+Enable-WindowsOptionalFeature -Online -FeatureName  Microsoft-Hyper-V-All
+
 #2.1.	Получить список коммандлетов работы с Hyper-V (Module Hyper-V)
+Get-Command -CommandType Cmdlet -Source Hyper-V 
+
 #2.2.	Получить список виртуальных машин 
+Get-VM
+
 #2.3.	Получить состояние имеющихся виртуальных машин
+Enable-VMResourceMetering -VMName Nikiforov-VM1
+Measure-VM -VMName Nikiforov-VM1
+
 #2.4.	Выключить виртуальную машину
+Stop-VM -Name VM1 -TurnOff -Force
+
 #2.5.	Создать новую виртуальную машину
+New-VM -Name VM4
+
 #2.6.	Создать динамический жесткий диск
+New-VHD -Path D:\VMS\test\dddisc.vhdx -Dynamic -SizeBytes 20Gb
+
 #2.7.	Удалить созданную виртуальную машину
+Remove-VM -Name VM4
