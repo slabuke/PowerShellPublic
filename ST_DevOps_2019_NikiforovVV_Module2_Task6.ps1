33da412ea9197e9363f92794d8a7c1bdfd2e5c53
Nikiforov Viacheslav <55716893+slabuke@users.noreply.github.com>
2019-10-08 22:48:53 +0300

    Update ST_DevOps_2019_NikiforovVV_Module2_Task6.ps1

--- ST_DevOps_2019_NikiforovVV_Module2_Task6.ps1
+++ ST_DevOps_2019_NikiforovVV_Module2_Task6.ps1
@@ -5,18 +5,29 @@ Get-NetIPConfiguration -Detailed | Select-Object -Property IPv4Address, Interfac
 #1.2.	Получить mac-адреса всех сетевых устройств вашего компьютера и удалённо.
 $Comp = @("VM1","VM2","VM3")
 Invoke-Command -ScriptBlock {
-    $PCName = $env:COMPUTERNAME
-    Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE | ForEach-Object {
-    Write-Host $PCName `t $_.Description `t $_.IPAddress `t $_.MACAddress
-    }
+     $PCName = $env:COMPUTERNAME
+     Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE | ForEach-Object {
+     Write-Host $PCName `t $_.Description `t $_.IPAddress `t $_.MACAddress
+         }
     } -ComputerName $Comp -Credential Administrator
-    Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE | ForEach-Object{ 
+    Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE | ForEach-Object { 
         Write-Host 'Description:' $_.Description -ForegroundColor Red -NoNewline `t;
         Write-Host 'IPAddress:' $_.IPAddress -ForegroundColor Cyan -NoNewline `t;
         Write-Host 'MACAddress:' $_.MACAddress -ForegroundColor Yellow 
     }
 
 #1.3.	На всех виртуальных компьютерах настроить (удалённо) получение адресов с DHСP.
+$Comp = @("VM1","VM2","VM3")
+Invoke-Command -ScriptBlock {
+$NICs = Get-WMIObject Win32_NetworkAdapterConfiguration | where{$_.IPEnabled -eq “TRUE”}
+Foreach($NIC in $NICs) {
+     $NIC.EnableDHCP()    
+     $NIC.SetDNSServerSearchOrder()    
+}
+IPConfig /all
+    } -ComputerName $Comp -Credential Administrator
+
+
 #1.4.	Расшарить папку на компьютере
 #1.5.	Удалить шару из п.1.4
 #1.6.	Скрипт входными параметрами которого являются Маска подсети и два ip-адреса. Результат  – сообщение (ответ) в одной ли подсети эти адреса.
