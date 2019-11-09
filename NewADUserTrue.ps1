bd6cd4c01e2c2c4e974ca5a7e357db2e820ac930
Nikiforov Viacheslav <55716893+slabuke@users.noreply.github.com>
2019-11-09 19:11:22 +0300

    Create NewADUserTrue.ps1

---
+++ NewADUserTrue.ps1
@@ -0,0 +1,35 @@
+﻿$ADUsers = Import-Csv -Delimiter ';' -Path "C:\Users\ASUS\Desktop\users1.csv"
+Invoke-Command -ScriptBlock {
+Param($ADUsers)
+Import-Module ActiveDirectory
+$OU = "OU=Lab_Users,DC=NV,DC=lab"
+foreach ($Users in $ADUsers) 
+{
+ $FirstName = $Users.FirstName
+ $LastName = $Users.LastName
+ $Username = $Users.FirstName + $users.LastName
+ $Password = $Users.DefaultPassword
+ $Department = $Users.Department
+
+ if (Get-ADUser -Filter {SamAccountName -eq $Username})
+ {
+ Write-Warning "A user account with username $Username already exist in Active Directory."
+ }
+ else
+ {
+  New-ADUser `
+  -SamAccountName $Username `
+  -UserPrincipalName "$Username@nv.lab" `
+  -Name "$Firstname $Lastname" `
+  -GivenName $Firstname `
+  -Surname $Lastname `
+  -Enabled $True `
+  -DisplayName "$Lastname, $Firstname" `
+  -Department $Department `
+  -Path $OU `
+  -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -ChangePasswordAtLogon $True
+  
+
+}
+}
+} -ComputerName DC01 -Credential Administrator -ArgumentList $ADUsers
